6356f5b654135bb7cae9ec350afc2b3cab0d0c62
abdurrohit <sheikhabdurrohit@gmail.com>
2025-04-30 01:19:15 +0530

    stabe dev branch

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "../../../../../node_modules/.prisma/client";
 
 const prisma = new PrismaClient();
 
